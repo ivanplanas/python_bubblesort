a1bc883c5f5d740a547cace71f6e0b8f18b780c3
ivanplanas <71102498+ivanplanas@users.noreply.github.com>
2020-09-12 12:18:46 -0400

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,17 @@
+numbers = [1,4,5,3,6,2,100,23,34,563,24] #Numbers to sort.
+length = len(numbers) # Calculates length of list.
+indexCount  = length - 1 #Takes length and converts to index count form for indexing.
+for x in range(0, indexCount): #first loop / main loop
+    for i in range(0, indexCount): #worker loop
+        if i+1 == indexCount: 
+            if numbers[i] > numbers[i+1]:
+                temp = numbers[i]
+                numbers[i] = numbers[i+1]
+                numbers[i+1] = temp
+                break
+        if numbers[i] > numbers[i+1]:
+            temp = numbers[i]
+            numbers[i] = numbers[i+1]
+            numbers[i+1] = temp 
+
+print(numbers) //prints bubble sorted list
